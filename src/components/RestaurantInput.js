4cbbb9f9b100b2e9a307dec808479c7adf9bc847
Matthew Robinson <matt_robinson1@yahoo.com>
2020-02-05 16:14:18 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -51,4 +51,4 @@ export class RestaurantInput extends Component {
 
 
 //connect this component by wrapping RestaurantInput below
-export default connect(mapState)
+export default connect(mapStateToProps)
